384ee331c9ec42ef28e119901cc6cb072d0254df
lyp741 <liangyupeng13@gmail.com>
2024-03-09 23:50:50 +0800

    add, bip vector string

--- main.cpp
+++ main.cpp
@@ -1,74 +1,105 @@
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/interprocess/containers/vector.hpp>
 #include <boost/interprocess/allocators/allocator.hpp>
-#include <string>
-#include <cstdlib> //std::system
+#include <boost/interprocess/containers/string.hpp>
 #include <iostream>
+#include <chrono>
+#include <unistd.h>
+#include <thread>
 using namespace boost::interprocess;
 
-//Define an STL compatible allocator of ints that allocates from the managed_shared_memory.
-//This allocator will allow placing containers in the segment
-typedef allocator<std::string, managed_shared_memory::segment_manager>  ShmemAllocator;
+// 定义所需的类型
+typedef managed_shared_memory::segment_manager segment_manager_t;
+typedef allocator<void, segment_manager_t> void_allocator;
+typedef allocator<char, segment_manager_t> char_allocator;
+typedef basic_string<char, std::char_traits<char>, char_allocator> shared_string;
+typedef allocator<shared_string, segment_manager_t> shared_string_allocator;
+typedef vector<shared_string, shared_string_allocator> shared_vector;
 
-//Alias a vector that uses the previous STL-like allocator so that allocates
-//its values from the segment
-typedef vector<std::string, ShmemAllocator> MyVector;
-
-//Main function. For parent process argc == 1, for child process argc == 2
-int main(int argc, char *argv[])
+int producer()
 {
-   if(argc == 1){ //Parent process
-      //Remove shared memory on construction and destruction
-      struct shm_remove
+   struct shm_remove
       {
          shm_remove() { shared_memory_object::remove("MySharedMemory"); }
          ~shm_remove(){ shared_memory_object::remove("MySharedMemory"); }
       } remover;
+    // 创建共享内存
+    managed_shared_memory segment(create_only, "MySharedMemory", 65536);
 
-      //Create a new segment with given name and size
-      managed_shared_memory segment(create_only, "MySharedMemory", 65536);
+    // 获取分配器
+    const shared_string_allocator alloc_inst(segment.get_segment_manager());
+    const char_allocator char_alloc_inst(segment.get_segment_manager());
 
-      //Initialize shared memory STL-compatible allocator
-      const ShmemAllocator alloc_inst (segment.get_segment_manager());
+    // 构造共享的vector
+    shared_vector* myVector = segment.construct<shared_vector>("MyVector")(alloc_inst);
 
-      //Construct a vector named "MyVector" in shared memory with argument alloc_inst
-      MyVector *myvector = segment.construct<MyVector>("MyVector")(alloc_inst);
-
-      for(int i = 0; i < 100; ++i)  //Insert data in the vector
-         myvector->push_back(std::to_string(i) + "hhh");
-
-      for(int i = 0; i < 100; ++i)
-         std::cout << myvector->at(i) << std::endl;
-
-      //Launch child process
-      std::string s(argv[0]); s += " child ";
-      if(0 != std::system(s.c_str()))
-         return 1;
-
-      //print vector
-      
-      //Check child has destroyed the vector
-      while(segment.find<MyVector>("MyVector").first)
-         ;
+    // 在 vector 中添加字符串
+    myVector->push_back(shared_string("Hello", char_alloc_inst));
+    myVector->push_back(shared_string("World", char_alloc_inst));
+   
+   for (int i=0; i<100; i++){
+      //每秒添加一个字符串
+      myVector->push_back(shared_string(std::to_string(i).c_str(), char_alloc_inst));
+      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
    }
-   else{ //Child process
-      //Open the managed segment
-      managed_shared_memory segment(open_only, "MySharedMemory");
 
-      //Find the vector using the c-string name
-      MyVector *myvector = segment.find<MyVector>("MyVector").first;
-      std::cout<< "myvector size = " << myvector->size() << std::endl;
-      //Use vector in reverse order
-      // std::sort(myvector->rbegin(), myvector->rend());
-      //Print vector
-      for(int i = 0; i < 100; ++i){
-         std::cout << "reading" << std::endl;
-         std::cout << myvector->at(i) << std::endl;
-         std::cout << "got" <<std::endl;
+   //  // 打印 vector 中的字符串
+   //  for (const auto& str : *myVector) {
+   //      std::cout << str << std::endl;
+   //  }
+
+    // 销毁共享内存
+    segment.destroy<shared_vector>("MyVector");
+
+    return 0;
+}
+
+int consumer()
+{
+   managed_shared_memory segment;
+   // 等待并打开已存在的共享内存
+   while (1){
+   try{
+      segment = managed_shared_memory(open_only, "MySharedMemory");
+      break;
+   }
+   catch(...){
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+   }
+   }
+    
+   // 获取共享内存中的vector
+   shared_vector* myVector = segment.find<shared_vector>("MyVector").first;
+
+   if (myVector) {
+      // 打印 vector 中的字符串
+      while(1){
+         if (myVector->size() > 0) {
+            std::cout << myVector->front() << std::endl;
+            myVector->erase(myVector->begin());
+            // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+         }
+         else{
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+         }
       }
-      //When done, destroy the vector from the segment
-      segment.destroy<MyVector>("MyVector");
+      
+   } else {
+      std::cerr << "Vector not found!" << std::endl;
    }
 
+   return 0;
+}
+
+int main(int argc, char* argv[]){
+   if (argc == 1){
+      //start a process to do the work
+      std::string s(argv[0]); s += " child ";
+      //启动子进程，不等待结束
+      producer();
+      return 0;
+      }
+   else
+      consumer();
    return 0;
 }
